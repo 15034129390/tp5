6e99dfd3c9fcc2f133122162acead207d148bc84
15034129390 <105425356+15034129390@users.noreply.github.com>
2022-05-16 09:36:22 +0800

    1

--- app/api/controller/BaseApi.php
+++ app/api/controller/BaseApi.php
@@ -6,7 +6,13 @@ namespace app\api\controller;
 
 
 class BaseApi{
+
     public $params;
 
-    
+    public $token;
+
+    public function __construct(){
+        $this->params = input();
+
+    }
 }
